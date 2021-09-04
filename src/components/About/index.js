329a5894a0808ee970f23e04920472a3be262a2e
Joseph <joe@uhhuhohyeah.com>
2021-09-04 15:56:52 -0700

    added gallery image folders and two size image sets

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,24 +3,24 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <h5 id="about">About me</h5>
-      <p className="text-muted basic">Born in Seattle, Washington, I'm a Los Angeles-based artist and web developer. My education includes degrees in philosophy, art(film), as well as a certificate in full-stack web development earned at UCLA's Coding Bootcamp. My web-related technical skills include:</p>
-        <ul className="text-muted">
+      <h5 id="about" className="text-head">About me</h5>
+      <p className="basic">Born in Seattle, Washington, I'm a Los Angeles-based artist and web developer. My education includes degrees in philosophy, art(film), as well as a certificate in full-stack web development earned at UCLA's Coding Bootcamp. My web-related technical skills include:</p>
+        <ul className="basic">
             <li>JavaScript</li>
             <li>React</li>
             <li>GraphQL</li>
             <li>SQL</li>
             <li>HTML/CSS</li>
         </ul>
-        <p className="text-muted">In addition to coding, I have experience in digital media using a range of software packages including:</p>
-        <ul className="text-muted">
+        <p className="basic">In addition to coding, I have experience in digital media using a range of software packages including:</p>
+        <ul className="basic">
             <li>Cinema4D (cg modeling & animation)</li>
             <li>AfterEffects</li>
             <li>Photoshop/Illustrator</li>
             <li>Premier Pro/Final Cut Pro</li>
             <li>Unreal (gaming/3d Interactive)</li>
         </ul>
-        <p className="text-muted">More detailed information can be found at the Resume link above.</p>
+        <p className="basic">More detailed information can be found at the Resume link above.</p>
 
 
     </section>
